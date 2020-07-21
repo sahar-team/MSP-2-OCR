0275a04a8df979fd383275bae45b1a7ac0858c2d
quentindeltour <quentindeltour@orange.fr>
2020-07-21 18:37:29 +0200

    new version of app using read api

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MSP-2
-MSP-2 Preparation
+MSP-2 Preparation : 
+
+# APP avec bounding boxes
+
+## Definir variable d'environnement : 
+https://docs.microsoft.com/fr-fr/azure/cognitive-services/cognitive-services-apis-create-account?tabs=multiservice%2Cwindows#configure-an-environment-variable-for-authentication
+Puis relancer l'éditeur pour que les changements fassent effet.
+
+## Lancer app flask
+
+exemple pour windows : 
+set FLASK_APP=main.py
+flask run
--- main.py
+++ main.py
@@ -5,6 +5,17 @@ import json
 import time
 from requests import get, post
 import urllib
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+from PIL import Image
+from io import BytesIO
+import os
+import random
+import string
+import glob
+from dotenv import load_dotenv
 
 app = Flask(__name__)
 
@@ -25,8 +36,14 @@ def add_comment():
         
         req = request.form.to_dict()
         comment = req["comment"]
-        text = get_text_from_url(comment)
-        return Response("<p>" + text + "</p>") 
+        #text = get_text_from_url(comment)
+        files = glob.glob('./static/output/*')
+        for f in files:
+            os.remove(f)
+        plt, texte = get_image_from_url(comment)
+        file_name = get_random_string(8)
+        plt.savefig('./static/output/{}.jpeg'.format(file_name), bbox_inches='tight') 
+        return render_template("resultats.html", text_result=texte, filename = '/output/' + file_name + '.jpeg')
         # return render_template("/index.html")
 
     
@@ -49,8 +66,8 @@ def add_comment():
 def get_text_from_url(url_image):
     
     url_image = url_image
-    subscription_key = '3878ef99c82a42c184eb1a2ef7c3773b'
-    endpoint = "https://quentin-computervision.cognitiveservices.azure.com/"
+    subscription_key = os.getenv("COMPUTER_VISION_SUBSCRIPTION_KEY")
+    endpoint = os.getenv("COMPUTER_VISION_ENDPOINT")
     ocr_url = endpoint + "vision/v3.0/ocr"
     
     headers = {"Ocp-Apim-Subscription-Key": subscription_key,
@@ -71,4 +88,56 @@ def get_text_from_url(url_image):
             texte += ' ' + str(line['words'][num]['text'])
             
     return texte
+
+def get_random_string(length):
+    letters = string.ascii_lowercase
+    result_str = ''.join(random.choice(letters) for i in range(length))
+    return result_str
     
+def get_image_from_url(url_image):
+    subscription_key = '3878ef99c82a42c184eb1a2ef7c3773b'
+    endpoint = 'https://quentin-computervision.cognitiveservices.azure.com/'
+    text_recognition_url = endpoint + "vision/v3.0/read/analyze"
+    # Set image_url to the URL of an image that you want to recognize.
+    image_url = url_image
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key}
+    data = {"url": image_url}
+    response = requests.post(
+        url = text_recognition_url, headers=headers, json=data)
+    
+    #Deuxieme commande get pour obtenir les resultats.
+    analysis = {}
+    poll = True
+    while (poll):
+        response_final = requests.get(
+            response.headers["Operation-Location"], headers=headers)
+        analysis = response_final.json()
+        time.sleep(1)
+        if ("analyzeResult" in analysis):
+            poll = False
+        if ("status" in analysis and analysis['status'] == 'failed'):
+            poll = False
+    
+    texte = ''
+    for index, line in enumerate(analysis['analyzeResult']['readResults'][0]['lines']):
+        for num in range(len(line['words'])):
+            texte += ' ' + str(line['words'][num]['text'])
+
+    polygons = []
+    if ("analyzeResult" in analysis):
+        # Extract the recognized text, with bounding boxes.
+        polygons = [(line["boundingBox"], line["text"])
+                    for line in analysis["analyzeResult"]["readResults"][0]["lines"]]
+        
+    image = Image.open(BytesIO(requests.get(image_url).content))
+    fig = plt.figure()
+    ax = plt.imshow(image)
+    for polygon in polygons:
+        vertices = [(polygon[0][i], polygon[0][i+1])
+                    for i in range(0, len(polygon[0]), 2)]
+        text = polygon[1]
+        patch = Polygon(vertices, closed=True, fill=False, linewidth=2, color='y')
+        ax.axes.add_patch(patch)
+        plt.text(vertices[0][0]+image.size[0], vertices[0][1], text, fontsize=20, va="top")
+    return fig, texte
+
